f4af11c1d4fde4846f0dc7e430f1e90eae43d375
ankit123rudra <ankit123rudra@gmail.com>
2017-10-27 21:44:01 +0530

    prime numbers
    Python program to print prime numbers.

---
+++ prime.py
@@ -0,0 +1,9 @@
+n= int(raw_input("enter the number upto which you want to print the prime numbers"))
+if n>0:
+  fori in range(2,n/2):
+    if(n%i==0):
+      print i
+      
+else:
+  print "please enter positive numbers"
+ 
